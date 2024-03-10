118761865073c6c62235a7b924fde5e91535b412
Tanju Özer <tanjuozer64@gmail.com>
2024-03-10 21:30:21 +0100

    build simple router and router page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/notFound/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./NotFound.module.css";
+
+function NotFound() {
+  return <div>NotFound</div>;
+}
+
+export default NotFound;
