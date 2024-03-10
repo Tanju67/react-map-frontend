f2fc27297b27dd38323ea0a7b7cdaf29aff636aa
Tanju Özer <tanjuozer64@gmail.com>
2024-03-11 00:36:30 +0100

    build discover section and its components

---
+++ src/components/home/DiscoverSection.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styles from "./DiscoverSection.module.css";
+import img1 from "../../assets/pexels-leah-newhouse-185933.jpg";
+import img2 from "../../assets/pexels-mihis-alex-21014.jpg";
+import img3 from "../../assets/pexels-porapak-apichodilok-346885.jpg";
+import Logo from "../../shared/UIElements/Logo";
+
+function DiscoverSection() {
+  return (
+    <div className={styles.section}>
+      <h2>
+        <Logo /> Discover to the world with us
+      </h2>
+      <div className={styles.item}>
+        <div className={styles.imgBox}>
+          <img src={img1} alt="img1" />
+        </div>
+        <div className={styles.textBox}>
+          <h3>Plan your travel</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            architecto aperiam natus maiores ex nemo amet voluptatum sunt quod
+            nam! Nisi voluptatum sed illum laborum iure corporis iste esse
+            inventore!
+          </p>
+        </div>
+      </div>
+      <div className={`${styles.item}`}>
+        <div className={styles.textBox}>
+          <h3>Plan your travel</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            architecto aperiam natus maiores ex nemo amet voluptatum sunt quod
+            nam! Nisi voluptatum sed illum laborum iure corporis iste esse
+            inventore!
+          </p>
+        </div>
+        <div className={styles.imgBox}>
+          <img src={img3} alt="img1" />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.imgBox}>
+          <img src={img2} alt="img1" />
+        </div>
+        <div className={styles.textBox}>
+          <h3>Plan your travel</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
+            architecto aperiam natus maiores ex nemo amet voluptatum sunt quod
+            nam! Nisi voluptatum sed illum laborum iure corporis iste esse
+            inventore!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DiscoverSection;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Home.module.css";
 import HeaderSection from "./HeaderSection";
+import DiscoverSection from "./DiscoverSection";
 
 function Home() {
   return (
     <div>
       <HeaderSection />
+      <DiscoverSection />
     </div>
   );
 }
