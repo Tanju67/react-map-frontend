e5c83c236011d3efc8b3acc0bcd17ff47e6a2d4e
Tanju Özer <tanjuozer64@gmail.com>
2024-03-11 02:21:05 +0100

    handle pricing section and its components

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./Home.module.css";
 import HeaderSection from "./HeaderSection";
 import DiscoverSection from "./DiscoverSection";
+import PricingSection from "./PricingSection";
 
 function Home() {
   return (
     <div>
       <HeaderSection />
       <DiscoverSection />
+      <PricingSection />
     </div>
   );
 }
